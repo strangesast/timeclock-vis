55745bd85a933489485ad31a6e2d126c80710123
Sam Zagrobelny <strangesast@gmail.com>
2020-02-25 10:07:57 -0500

    questionable

--- server/init.py
+++ server/init.py
@@ -19,110 +19,90 @@ import models
 from util import get_async_rpc_connection, merge_nearby_shifts, parse_timecards_2, get_mysql_db, get_mongo_db
 
 
-async def init(config):
+async def init(config, force=False):
     mysql_client = await get_mysql_db(config['MYSQL'])
     mongo_client = await get_mongo_db(config['MONGO'])
 
-    proxy = get_async_rpc_connection(config['AMG'])
+    amg_rpc_proxy = get_async_rpc_connection(config['AMG'])
+
+    mongo_db = mongo_client.timeclock
+
+    collection_names = await mongo_db.list_collection_names()
+
+    mysql_cursor = await mysql_client.cursor(aiomysql.DictCursor)
+    if force or 'employees' not in collection_names:
+        await mysql_cursor.execute('select id,Code,Name,MiddleName,LastName,HireDate from tam.inf_employee')
+
+        employee_ids = [];
+        employees = {}
+        async for i, employee in enumerate(wrap_fetchone(mysql_cursor)):
+            color = models.EmployeeShiftColor(i % len(models.EmployeeShiftColor))
+            employee_id = str(employee['id'])
+            employee_ids.append(employee_id)
+            employee['id'] = employee_id
+            employee['Color'] = color
+            employees[employee['id']] = employee;
+
+        await mongo_db.drop_collection('employees')
+        col = mongo_db.get_collection('employees')
+        await col.create_index('id', unique=True)
+        await col.insert_many(employees.values())
+    else:
+        employees = await mongo_db.employees.find({}).to_list(2000)
+        employee_ids = [empl['id'] for empl in employees]
+
+    if force or 'polls' not in collection_names:
+        await mongo_db.drop_collection('polls')
+
+    latest_poll = await mongo_db.polls.find_one({}, sort=[('date', pymongo.DESCENDING)])
+    if latest_poll is None:
+        await mysql_cursor.execute('select StartTime from tam.polllog order by StartTime desc')
+    else:
+        await mysql_cursor.execute('select StartTime from tam.polllog where StartTime > %s order by StartTime desc', (latest_poll['date'],))
+
+    if mysql_cursor.rowcount:
+        polls = [{'date': date} for date, *_ in await mysql_cursor.fetchall()]
+        result = await mongo_client.timeclock.polls.insert_many(polls)
+        latest_poll = await mongo_db.polls.find_one({'_id': result.inserted_ids[0]})
 
 
-    cur = await mysql_client.cursor(aiomysql.DictCursor)
-    await cur.execute('select id,Code,Name,MiddleName,LastName,HireDate from tam.inf_employee')
-
-    employee_ids = [];
-    employees = {}
-    async for i, employee in enumerate(wrap_fetchone(cur)):
-        color = models.EmployeeShiftColor(i % len(models.EmployeeShiftColor))
-        employee_id = str(employee['id'])
-        employee_ids.append(employee_id)
-        employee['id'] = employee_id
-        employee['Color'] = color
-        employees[employee['id']] = employee;
-
-    await mongo_client.timeclock.drop_collection('employees')
-    await mongo_client.timeclock.employees.create_index('id', unique=True)
-    result = await mongo_client.timeclock.employees.insert_many(employees.values())
-
-
-
-    # check polllog
-    cur = await mysql_client.cursor()
-    await cur.execute('select StartTime,Messages from tam.polllog order by StartTime desc limit 1')
-    row = await cur.fetchone()
-    if row is None:
-        raise Exception('no polls? somethings fucked');
-    last_poll = row[0]
-
-    doc = await mongo_client.timeclock.polls.find_one({}, sort=[('date', pymongo.DESCENDING)])
-    if doc is None:
-        await cur.execute('select StartTime from tam.polllog')
-        polls = await cur.fetchall()
-        polls = [{'date': date} for date, *_ in polls]
-        print(len(polls))
-        await mongo_client.timeclock.polls.insert_many(polls)
-    elif last_poll > doc['date']:
-        await cur.execute('select StartTime from tam.polllog where StartTime > %s', (doc['date'],))
-        if cur.rowcount:
-            polls = await cur.fetchall()
-            polls = [{'date': date} for date, *_ in polls]
-            print(len(polls))
-            await mongo_client.timeclock.polls.insert_many(polls)
-
-    print(f'Last poll {row[0]}')
-
-    #cur = await mysql_client.cursor(aiomysql.DictCursor)
-    #await cur.execute('select id,inf_employee_id,Date from tam.tr_clock order by inf_employee_id,Date asc')
-
-    now = datetime.now()
-    today = now.replace(hour=0, minute=0, second=0, microsecond=0)
-    interval = timedelta(weeks=2)
-    min_date = today - timedelta(days=365)
-    min_date = min_date - timedelta(days=min_date.weekday() - 1)
-
-    shifts = []
-    while min_date < today:
-        max_date = min_date + interval
-        print(min_date, max_date)
-        employee_timecards = await proxy.GetTimecards([int(s) for s in employee_ids], min_date, max_date, False)
-        for each in employee_timecards:
-            employee_id, timecards = each['EmployeeId'], each['Timecards']
-
-            incomplete_shifts = []
-
-            #for components in merge_nearby_shifts(parse_timecards(employee_id, timecards)):
-            for components in parse_timecards_2(employee_id, timecards):
-                start_date = components[0]['start']
-                end_date = components[-1]['end']
-                is_complete = end_date is not None
-                shift_state = models.ShiftState.Complete if is_complete else models.ShiftState.Incomplete
-                total_duration = timedelta()
-                if is_complete:
-                    for component in components:
-                        total_duration += component['end'] - component['start']
-                is_flagged = True if not is_complete and ((total_duration is not None and total_duration > timedelta(hours=24)) or (now - start_date) > timedelta(hours=24)) else False
-                shift = {
-                    'flagged': is_flagged,
-                    'employee': str(employee_id),
-                    'components': components,
-                    'start': start_date,
-                    'end': end_date,
-                    'duration': total_duration,
-                    'state': shift_state.value
-                }
-                if not is_complete:
-                    incomplete_shifts.append(shift)
-                shifts.append(shift)
-
-            for shift in incomplete_shifts[:-1]:
-                shift['flagged'] = True
-
-        min_date = max_date
-
-    await mongo_client.timeclock.drop_collection('shifts')
     type_registry = TypeRegistry(fallback_encoder=timedelta_encoder)
     codec_options = CodecOptions(type_registry=type_registry)
-    collection = mongo_client.timeclock.get_collection('shifts', codec_options=codec_options)
-    result = await collection.insert_many(shifts)
+    shifts_collection = mongo_db.get_collection('shifts', codec_options=codec_options)
+
+    if force or 'shifts' not in collection_names:
+        await mongo_db.drop_collection('shifts')
+        await mongo_db.drop_collection('sync_history')
+        # problematic
+        #await shifts_collection.create_index([('start', pymongo.DESCENDING), ('employee', pymongo.ASCENDING)], unique=True)
+        await shifts_collection.create_index('start')
+        await shifts_collection.create_index('end')
+        await shifts_collection.create_index('employee')
+
+    latest_sync = await mongo_db.sync_history.find_one({}, sort=[('date', pymongo.DESCENDING)])
+
+    now = datetime.now()
+    interval = timedelta(weeks=2)
+
+    if latest_sync is None:
+        min_date = get_sunday(now - timedelta(days=365))
+
+    # no poll since last sync
+    #elif latest_sync['poll']['date'] >= latest_poll['date']:
+    #    return
+    else:
+        min_date = get_sunday(now)
+
+    shifts = []
+    while min_date < now:
+        max_date = min_date + interval
+        print(f'{min_date} - {max_date}')
+        shifts = await get_employee_shifts(amg_rpc_proxy, employee_ids, (min_date, max_date))
+        if len(shifts):
+            await shifts_collection.insert_many(shifts)
+        min_date = max_date
+
+    await mongo_db.sync_history.insert_one({'poll': latest_poll, 'date': now, 'min': min_date})
 
     last_employee = None
     last_start = None
@@ -130,7 +110,7 @@ async def init(config):
     last_id = None
     last_duration = None
     duplicates = []
-    async for doc in collection.find().sort([('employee', pymongo.ASCENDING), ('start', pymongo.ASCENDING)]):
+    async for doc in shifts_collection.find().sort([('employee', pymongo.ASCENDING), ('start', pymongo.ASCENDING)]):
         _id = doc['_id']
         employee = doc['employee']
         if employee != last_employee:
@@ -159,7 +139,7 @@ async def init(config):
         last_duration = duration
 
     if len(duplicates):
-        await collection.delete_many({'_id': {'$in': duplicates}});
+        await shifts_collection.delete_many({'_id': {'$in': duplicates}});
 
     # calculate count, avg / stdDev for start, end, duration of shift
     pipeline = [
@@ -214,30 +194,68 @@ async def init(config):
       {'$merge': {'into': 'employees', 'on': 'id'}}
     ]
 
-    async for doc in collection.aggregate(pipeline):
+    # add stats for each employee
+    async for doc in shifts_collection.aggregate(pipeline):
         pass
 
 
-    await proxy.close()
-    await cur.close()
+    await amg_rpc_proxy.close()
+    await mysql_cursor.close()
     mongo_client.close()
 
 
-"""
-read polllog collection
-if exists, get most recent poll
-query for new polls
-if not exists, query for whole set
-"""
+async def get_employee_shifts(proxy, employee_ids, date_range):
+    ids = [int(s) for s in employee_ids]
+    employee_timecards = await proxy.GetTimecards(ids, *date_range, False)
+    now = datetime.now()
+    shifts = []
+    for each in employee_timecards:
+        employee_id, timecards = each['EmployeeId'], each['Timecards']
+
+        for components in parse_timecards_2(employee_id, timecards):
+            start_date = components[0]['start']
+            end_date = components[-1]['end']
+            is_complete = end_date is not None
+
+            total_duration = reduce(lambda t, c: (c['end'] or now) - c['start'], components, timedelta())
+
+            shift_state = models.ShiftState.Complete if is_complete else models.ShiftState.Incomplete
+
+            is_flagged = False
+            if not is_complete and (total_duration and total_duration > timedelta(hours=24)):
+                is_flagged = True
+            elif not is_complete and (now - start_date) > timedelta(hours=24):
+                is_flagged = True
+
+            shifts.append({
+                'flagged': is_flagged,
+                'employee': str(employee_id),
+                'components': components,
+                'start': start_date,
+                'end': end_date,
+                'duration': total_duration if is_complete else timedelta(),
+                'state': shift_state
+            })
+    return shifts
+
+
 
 def timedelta_encoder(value):
     if isinstance(value, timedelta):
         return int(value.total_seconds() * 1000)
+    if isinstance(value, Enum):
+        return value.value
     return value
 
 
-async def wrap_fetchone(cur: aiomysql.Cursor):
-    while (item := await cur.fetchone()):
+def get_sunday(dt: datetime):
+    dt = dt.replace(hour=0, minute=0, second=0, microsecond=0)
+    dt = dt - timedelta(days=(dt.weekday() + 1) % 7)
+    return dt
+
+
+async def wrap_fetchone(mysql_cursor: aiomysql.Cursor):
+    while (item := await mysql_cursor.fetchone()):
         yield item
 
 
@@ -269,4 +287,4 @@ if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read(config_path)
 
-    asyncio.run(init(config))
+    asyncio.run(init(config, True))
--- server/server.py
+++ server/server.py
@@ -4,6 +4,7 @@ import asyncio
 import weakref
 import pymongo
 import configparser
+from pprint import pprint
 import motor.motor_asyncio
 from functools import reduce
 from bson.json_util import dumps
@@ -17,158 +18,6 @@ import models
 routes = web.RouteTableDef()
 EMPLOYEE_IDS = ['50', '53', '71', '61', '82', '73', '55', '72', '66', '62', '69', '67', '80', '79', '57', '51', '70', '74', '54', '56', '58', '59', '64', '65']
 
-'''
-@routes.get('/data/shifts')
-async def get_shifts(request):
-    req_q = request.query
-    # limit response count?
-    # "after" some id
-    # "minDate", "maxDate"
-    query = {}
-
-    if 'minDate' in req_q and 'maxDate' in req_q:
-        try:
-            minDate, maxDate = req_q['minDate'], req_q['maxDate']
-            minDate, maxDate = map(lambda s: s.replace('Z', '+00:00'), [minDate, maxDate])
-            minDate, maxDate = map(datetime.fromisoformat, [minDate, maxDate])
-        except ValueError as e:
-            return web.HTTPBadRequest(body='invalid date')
-
-        query = {'$or': [
-            {'start': {'$gt': minDate, '$lt': maxDate}},
-            {'start': {'$gt': minDate}, 'end': None},
-            {'end':   {'$gt': minDate, '$lt': maxDate}},
-            {'start': {'$lt': minDate}, 'end': {'$gt': maxDate}},
-        ]}
-
-    if 'employee' in req_q:
-        try:
-            employee_id = req_q['employee']
-            employee_id = int(employee_id)
-            employee_id = str(employee_id)
-        except ValueError as e:
-            return web.HTTPBadRequest(body='invalid employee id')
-        query['employee'] = employee_id
-        
-    cursor = request.app['db'].timeclock.shifts.find(query, {"_id": 0}).sort('_id', pymongo.ASCENDING)
-    shifts = await cursor.to_list(None)
-
-    employee_ids = sorted(list(set(s['employee'] for s in shifts)))
-
-    cursor = request.app['db'].timeclock.employees.find({'id': {'$in': employee_ids}}, {"_id": 0})
-
-    employees = {};
-    async for employee in cursor:
-        employees[employee['id']] = employee;
-
-    #interface ShiftsResponse {
-    #  shifts: Shift[];
-    #  employees: Map<Employee>;
-    #  employeeIds: EmployeeID[];
-    #}
-    #export interface EmployeeShiftsResponse {
-    #  employee: Employee;
-    #  shifts: Shift[];
-    #}
-
-    response: models.EmployeeShiftsResponse = {
-        'shifts': shifts,
-        'employeeIds': employee_ids,
-        'employees': employees,
-    }
-    return json_response(response)
-    # need encoder
-    #return web.json_response({'shifts': shifts})
-'''
-
-default_min_date = datetime.combine(date.today() - timedelta(days=2), datetime.min.time())
-default_max_date = datetime.combine(date.today() + timedelta(days=2), datetime.min.time())
-default_employee_props = [
-  'LastName',
-  'MiddleName',
-  'FullName',
-  'Badge',
-  'Active',
-  'Code',
-  'Id',
-  'Name',
-]
-
-
-'''
-@routes.get('/data/shifts')
-async def get_shifts(request):
-    """
-    borked.  API returns inconsistent results based on time range
-    """
-    q = parse_qs(request.query)
-    if employee_id := q.get('employee'):
-        employees_list = await request.app['proxy'].GetEmployees([employee_id])
-    else:
-        employees_list = await request.app['proxy'].GetAllEmployeesShort()
-
-    # must be ints for next api call
-    employee_ids = [empl['Id'] for empl in employees_list]
-
-    # proxy returns 'None' for null values
-    employees_list = [{k: v if (v := empl.get(k)) != 'None' else None for k in default_employee_props}
-            for empl in employees_list]
-
-    # timezone is wrong
-    min_date = d - timedelta(hours=10) if (d := q.get('minDate')) else default_min_date;
-    min_date = min_date.replace(hour=0, minute=0, second=0, microsecond=0)
-    max_date = d - timedelta(hours=10) if (d := q.get('maxDate')) else default_max_date;
-    max_date = max_date.replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=1)
-
-    included_employee_ids = set()
-    employee_timecards = await request.app['proxy'].GetTimecards(employee_ids, min_date, max_date, False)
-    shifts = []
-    for each in employee_timecards:
-        employee_id, timecards = each['EmployeeId'], each['Timecards']
-        for components in merge_nearby_shifts(parse_timecards(employee_id, timecards)):
-            included_employee_ids.add(employee_id)
-            start_date = components[0][0]
-            end_date = components[-1][1]
-            id = f'{employee_id}_{start_date.timestamp():.0f}'
-            is_complete = end_date is not None
-            shift_state = models.ShiftState.Complete if is_complete else models.ShiftState.Incomplete
-            total_duration = reduce(lambda acc, cv: cv[1] - cv[0] + acc, components, timedelta()) if is_complete else None
-            components = [{'start': start, 'end': end, 'duration': end and end - start, 'id': f'{employee_id}_{start.timestamp():.0f}'}
-                    for start, end in components]
-            shift = {
-                'id': id,
-                'employee': str(employee_id),
-                'components': components,
-                'start': start_date,
-                'end': end_date,
-                'duration': total_duration,
-                'state': shift_state,
-                }
-            shifts.append(shift)
-
-    shifts.sort(key = lambda v: v['start'])
-
-    # for now return the same contents / order of employee ids
-    employee_ids = list(dict.fromkeys(s['employee'] for s in shifts)) if employee_id else EMPLOYEE_IDS
-
-    employees = {}
-    for employee in employees_list:
-        employee_id = employee['Id']
-        key = str(employee_id)
-        #if employee_id in included_employee_ids:
-        employees[key] = {
-            'id': key,
-            'name': f'{employee["Name"]} {employee["LastName"]}',
-            'color': models.EmployeeShiftColor(employee_id % len(models.EmployeeShiftColor)),
-        }
-
-    return json_response({
-        'employees': employees,
-        'employeeIds': employee_ids,
-        'shifts': shifts
-    })
-'''
-
 
 @routes.get('/data/shifts')
 async def get_shifts(request):
@@ -176,23 +25,28 @@ async def get_shifts(request):
 
 
     min_date, max_date = q.get('minDate'), q.get('maxDate')
-
-    if min_date is None:
-        min_date = datetime.now() - timedelta(hours=24)
-    if max_date is None:
-        max_date = datetime.now() + timedelta(hours=24)
+    limit = 2000
 
     db = request.app['db'].timeclock;
 
-    query = {
-        '$or': [
-            {'start': {'$gt': min_date, '$lt': max_date}},
-            {'$and': [{'end': None}, {'start': {'$lt': max_date}}]},
-            {'end': {'$gt': min_date, '$lt': max_date}},
-            {'$and': [{'start': {'$lt': min_date}}, {'end': {'$gt': max_date}}]},
-        ],
-        'flagged': False,
-    }
+    if max_date is None and min_date is None:
+        query = {}
+    elif min_date is None:
+        query = {'start': {'$lt': max_date}}
+    elif max_date is None:
+        query = {'$or': [{'start': {'$gt': min_date}, 'end': None}, {'end': {'$gt': min_date}}]}
+    else: 
+        query = {
+            '$or': [
+                {'start': {'$gt': min_date, '$lt': max_date}},
+                {'$and': [{'end': None}, {'start': {'$lt': max_date}}]},
+                {'end': {'$gt': min_date, '$lt': max_date}},
+                {'$and': [{'start': {'$lt': min_date}}, {'end': {'$gt': max_date}}]},
+            ],
+        }
+
+    # uggglyy
+    query['flagged'] = False
 
     if employee_id := q.get('employee'):
         employee = await db.employees.find_one({'id': employee_id})
@@ -204,7 +58,8 @@ async def get_shifts(request):
         employees = await db.employees.find({}).to_list(100)
         employees = {id: employee for employee in employees if (id := employee.get('id'))}
 
-    shifts = await db.shifts.find(query).to_list(100000)
+    pprint(query)
+    shifts = await db.shifts.find(query).sort([('start', pymongo.ASCENDING)]).limit(limit).to_list(limit)
 
     return json_response({
         'employees': employees,
@@ -222,60 +77,6 @@ def default(o):
         return o.isoformat()
     if isinstance(o, timedelta):
         return int(o.total_seconds() * 1000)
-
-
-async def on_shutdown(app):
-    for ws in set(app['websockets']):
-        await ws.close(code=WSCloseCode.GOING_AWAY,
-                       message='Server shutdown')
-    #await app['proxy'].close()
-    app['db'].close()
-
-
-async def background(app):
-    try:
-        while True:
-            # check db
-            changes = False
-            state = {}
-            if changes:
-                for ws in set(app['websockets']):
-                    await ws.send_str(json.dumps(state, default=default))
-            await asyncio.sleep(1000)
-    except asyncio.CancelledError:
-        pass
-    finally:
-        pass
-
-
-async def main(config):
-    await init(config)
-
-    app = web.Application()
-    app['websockets'] = weakref.WeakSet()
-
-    app['db'] = await get_mongo_db(config['MONGO'])
-
-    #app['proxy'] = get_async_rpc_connection(config['AMG'])
-
-    app.add_routes(routes)
-    app.on_shutdown.append(on_shutdown)
-
-    runner = web.AppRunner(app)
-    await runner.setup()
-
-    port = os.environ.get('SERVER_BIND_PORT') or config['SERVER'].get('BIND_PORT') or '8081'
-    host = os.environ.get('SERVER_BIND_HOST') or config['SERVER'].get('BIND_HOST') or '0.0.0.0'
-    site = web.TCPSite(runner, host, port)
-    await site.start()
-
-    try:
-        print(f'server running on {host}:{port}')
-        await asyncio.create_task(background(app))
-    finally:
-        print('shutting down')
-        await runner.cleanup()
-
 
 def utc_to_local(dt: datetime):
     return dt.replace(tzinfo=timezone.utc).astimezone(tz=None)
@@ -299,11 +100,53 @@ def parse_qs(query):
     return result
 
 
-if __name__ == '__main__':
+async def background(app):
+    try:
+        while True:
+            # check db
+            changes = False
+            state = {}
+            if changes:
+                for ws in set(app['websockets']):
+                    await ws.send_str(json.dumps(state, default=default))
+            await asyncio.sleep(1000)
+    except asyncio.CancelledError:
+        pass
+    finally:
+        pass
+
+
+async def on_shutdown(app):
+    await app['background'].cancel()
+    for ws in set(app['websockets']):
+        await ws.close(code=WSCloseCode.GOING_AWAY,
+                       message='Server shutdown')
+    #await app['proxy'].close()
+    app['db'].close()
+
+
+async def on_startup(app):
+    app['background'] = asyncio.create_task(background(app))
+    #app['proxy'] = get_async_rpc_connection(config['AMG'])
+
+
+async def main():
     configpath = os.path.join(os.path.dirname(__file__), 'config.ini')
     if not os.path.isfile(configpath):
         raise RuntimeError('no config file found')
     config = configparser.ConfigParser()
     config.read(configpath)
 
-    asyncio.run(main(config))
+    app = web.Application()
+    app['websockets'] = weakref.WeakSet()
+
+    app.add_routes(routes)
+    app['db'] = await get_mongo_db(config['MONGO'])
+    app.on_startup.append(on_startup)
+    app.on_shutdown.append(on_shutdown)
+
+    return app
+
+
+if __name__ == '__main__':
+    web.run_app(main())
