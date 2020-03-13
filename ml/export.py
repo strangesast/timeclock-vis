4d2427d4293ef0ba4002c5293c8b9545b6777c30
Sam Zagrobelny <strangesast@gmail.com>
2020-03-12 21:29:26 -0400

    playing with ml from examples

--- ml/export.py
+++ ml/export.py
@@ -1,21 +1,38 @@
+import asyncio
+import motor.motor_asyncio
+from bson.json_util import dumps
+
+from pprint import pprint
+
+VALID_EMPLOYEE_PIPELINE = [
+  {'$group': {'_id': '$employee', 'count': {'$sum': 1}}},
+  {'$match': {'count': {'$gte': 20}}},
+  {'$project': {'id': '$_id', '_id': 0}},
+]
+
 PIPELINE = [
+    # use only completed shifts
     {'$match': {'end': {'$ne': None}}},
     {'$addFields': {
-        'Tstart': {'$dateToParts': {'date': '$start', 'timezone': 'America/New_York'}},
-        'Tend': {'$dateToParts': {'date': '$end', 'timezone': 'America/New_York'}},
-        'dow': {'$dayOfWeek': {'date': '$start', 'timezone': 'America/New_York'}},
+        'startHour': {'$dateToParts': {'date': '$start', 'timezone': 'America/New_York'}},
+        'endHour': {'$dateToParts': {'date': '$end', 'timezone': 'America/New_York'}},
+        'dayOfWeek': {'$dayOfWeek': {'date': '$start', 'timezone': 'America/New_York'}},
     }},
+    # calculate seconds in day from hour/minute/second
     {'$addFields': {
-        'Tstart': {'$sum': ['$Tstart.second', {'$multiply': ['$Tstart.minute', 60]}, {'$multiply': ['$Tstart.hour', 3600]}]},
-        'Tend': {'$sum': ['$Tend.second', {'$multiply': ['$Tend.minute', 60]}, {'$multiply': ['$Tend.hour', 3600]}]},
+        'startHour': {'$sum': ['$startHour.second', {'$multiply': ['$startHour.minute', 60]}, {'$multiply': ['$startHour.hour', 3600]}]},
+        'endHour': {'$sum': ['$endHour.second', {'$multiply': ['$endHour.minute', 60]}, {'$multiply': ['$endHour.hour', 3600]}]},
     }},
+    # if overnight shift, fix
     {'$addFields': {
-        'Tend': {'$cond': {'if': {'$gte': ['$Tstart', '$Tend']}, 'then': {'$sum': ['$Tend', 8.64e4]}, 'else': '$Tend'}}
+        'endHour': {'$cond': {'if': {'$gte': ['$startHour', '$endHour']}, 'then': {'$sum': ['$endHour', 8.64e4]}, 'else': '$endHour'}}
     }},
+    # use hours instead of seconds
     {'$addFields': {
-        'Tstart': {'$divide': ['$Tstart', 3600]},
-        'Tend': {'$divide': ['$Tend', 3600]},
+        'startHour': {'$divide': ['$startHour', 3600]},
+        'endHour': {'$divide': ['$endHour', 3600]},
     }},
+    # join with components
     {'$unwind': '$components'},
     {'$lookup': {
         'from': 'components',
@@ -31,8 +48,10 @@ PIPELINE = [
     }},
     {'$addFields': {'root.components': '$components'}},
     {'$replaceRoot': {'newRoot': '$root'}},
+    # recalculate cumulative duration from components
     {'$addFields': {'duration': {'$map': {'input': '$components', 'in': {'$subtract': ['$$this.end', '$$this.start']}}}}},
     {'$addFields': {'duration': {'$sum': '$duration'}}},
+    # use hours instead of milliseconds
     {'$addFields': {'duration': {'$divide': ['$duration', 3.6e6]}}},
     {'$sort': {'start': 1}},
     {'$addFields': {
@@ -41,22 +60,32 @@ PIPELINE = [
             'year': {'$year': {'date': '$start', 'timezone': 'America/New_York'}},
         }
     }},
+    # group by year & week
     {'$group': {
         '_id': {'employee': '$employee', 'week': '$weekYear'},
         'shifts': {'$push': '$$ROOT'},
     }},
+    # break out totals duration each shift
     {'$addFields': {
         'shiftLengths': {'$map': {'input': '$shifts', 'in': '$$this.duration'}},
     }},
+    # calculate running total hours
     {'$addFields': {
-        'shiftWeekHours': {'$reduce': {'input': '$shiftLengths', 'initialValue': [], 'in': {'$concatArrays': ['$$value', [{'$sum': [{'$arrayElemAt': ['$$value', -1]}, '$$this']}]]}}}
+        'shiftWeekHours': {'$reduce': {
+            'input': '$shiftLengths',
+            'initialValue': [],
+            'in': {'$concatArrays': ['$$value', [{'$sum': [{'$arrayElemAt': ['$$value', -1]}, '$$this']}]]}
+            }}
     }},
+    # offset counts to not include current shift
     {'$addFields': {
         'shiftWeekHours': {'$concatArrays': [[0], '$shiftWeekHours']},
     }},
+    # replace number with object for later mergeObjects
     {'$addFields': {
         'shiftWeekHours': {'$map': {'input': '$shiftWeekHours', 'in': {'weekHours': '$$this'}}},
     }},
+    # zip durations with shifts
     {'$addFields': {
         'shifts': {'$zip': {'inputs': ['$shifts', '$shiftWeekHours']}},
     }},
@@ -66,4 +95,25 @@ PIPELINE = [
     {'$project': {'shifts': 1}},
     {'$unwind': '$shifts'},
     {'$replaceRoot': {'newRoot': '$shifts'}},
+    {'$project': {'components': 0, 'state': 0, 'row': 0, 'weekYear': 0}},
 ]
+
+
+async def main():
+    host, port = 'localhost', 27017
+    url = f'mongodb://{host}:{port}'
+    conn = motor.motor_asyncio.AsyncIOMotorClient(url)
+    db = conn.timeclock
+
+    employee_ids = [doc['id'] for doc in await db.shifts.aggregate(VALID_EMPLOYEE_PIPELINE).to_list(None)]
+
+    with open('data.json', 'w') as f:
+        pipeline = [{'$match': {'employee': {'$in': employee_ids}}}, *PIPELINE]
+        data = await db.shifts.aggregate(pipeline).to_list(None)
+        f.write(dumps(data, sort_keys=True, indent=4))
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    loop.close()
---
+++ ml/main.py
@@ -0,0 +1,134 @@
+import pathlib
+from bson.json_util import loads
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import tensorflow_docs as tfdocs
+import tensorflow_docs.plots
+import tensorflow_docs.modeling
+
+with open('data.json', 'r') as f:
+    raw_dataset = pd.DataFrame(loads(f.read()))
+
+
+dataset = raw_dataset.copy()
+
+
+dataset = dataset.drop('_id', 1)
+dataset = dataset.drop('start', 1)
+dataset = dataset.drop('end', 1)
+
+# temp
+#dataset = dataset.drop('employee', 1)
+#dataset = dataset.drop('dayOfWeek', 1)
+dataset = dataset.drop('endHour', 1)
+dataset = dataset.drop('startHour', 1)
+dataset = dataset.drop('weekHours', 1)
+#dataset = dataset.drop('duration', 1)
+
+
+#dataset['weekHours'] = 40 - dataset['weekHours']
+
+
+#dataset['dayOfWeek'] = dataset['dayOfWeek'].map({i+1: s for i, s in enumerate(['Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri'])})
+dataset = pd.get_dummies(dataset, prefix='', prefix_sep='')
+
+#print(list(dataset))
+print(dataset.tail())
+
+train_dataset = dataset.sample(frac=0.8,random_state=0)
+test_dataset = dataset.drop(train_dataset.index)
+
+#sns.pairplot(train_dataset[['startHour', 'endHour', 'duration', 'weekHours']], diag_kind='kde')
+#plt.show()
+
+train_stats = train_dataset.describe()
+train_stats.pop('duration')
+train_stats = train_stats.transpose()
+
+
+train_labels = train_dataset.pop('duration')
+test_labels = test_dataset.pop('duration')
+
+def norm(x):
+  return (x - train_stats['mean']) / train_stats['std']
+
+normed_train_data = norm(train_dataset)
+normed_test_data = norm(test_dataset)
+
+print(normed_train_data.tail())
+#print(train_dataset.keys())
+
+def build_model():
+    model = keras.Sequential([
+      layers.Dense(64, activation='relu', input_shape=[len(train_dataset.keys())]),
+      layers.Dense(64, activation='relu'),
+      layers.Dense(1)
+    ])
+    
+    optimizer = tf.keras.optimizers.RMSprop(0.001)
+    
+    model.compile(loss='mse',
+                  optimizer=optimizer,
+                  metrics=['mae', 'mse'])
+    return model
+
+model = build_model()
+
+EPOCHS = 200
+
+# The patience parameter is the amount of epochs to check for improvement
+early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
+
+early_history = model.fit(normed_train_data, train_labels,
+                    epochs=EPOCHS, validation_split = 0.2, verbose=0,
+                    callbacks=[early_stop, tfdocs.modeling.EpochDots()])
+
+example_batch = normed_train_data[:10]
+example_result = model.predict(example_batch)
+print(example_result)
+
+#print(model.summary())
+
+#history = model.fit(
+#  normed_train_data, train_labels,
+#  epochs=EPOCHS, validation_split = 0.2, verbose=0,
+#  callbacks=[tfdocs.modeling.EpochDots()])
+#
+#hist = pd.DataFrame(history.history)
+#hist['epoch'] = history.epoch
+#print(hist.tail())
+
+
+#plotter = tfdocs.plots.HistoryPlotter(smoothing_std=2)
+#plotter.plot({'Basic': early_history}, metric = "mean_absolute_error")
+#plt.ylim([0, 2])
+#plt.ylabel('MAE [startHour]')
+#
+#plt.show()
+
+loss, mae, mse = model.evaluate(normed_test_data, test_labels, verbose=2)
+print("Testing set Mean Abs Error: {:5.2f} hours (duration)".format(mae))
+
+
+test_predictions = model.predict(normed_test_data)
+test_predictions = test_predictions.flatten()
+
+a = plt.axes(aspect='equal')
+plt.scatter(test_labels, test_predictions)
+plt.xlabel('True Values [hours]')
+plt.ylabel('Predictions [hours]')
+lims = [0, 20]
+plt.xlim(lims)
+plt.ylim(lims)
+_ = plt.plot(lims, lims)
+
+#error = test_predictions - test_labels
+#plt.hist(error, bins = 25)
+#plt.xlabel("Prediction Error [hours]")
+#_ = plt.ylabel("Count")
+plt.show()
