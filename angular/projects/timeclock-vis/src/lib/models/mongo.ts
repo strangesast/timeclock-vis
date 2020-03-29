661aebf218fac55c4ef1cbc934dc049d9039c278
Sam Zagrobelny <strangesast@gmail.com>
2020-03-29 15:24:50 -0400

    removed angular for now

--- angular/projects/timeclock-vis/src/lib/models/mongo.ts
+++
@@ -1,3 +0,0 @@
-export interface MongoObject {
-  _id: string;
-}
--- angular/projects/timeclock-vis/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of timeclock-vis
- */
-
-export * from './lib/timeclock-vis.service';
-export * from './lib/timeclock-vis.component';
-export * from './lib/timeclock-vis.module';
