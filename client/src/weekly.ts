b7e025bc11894223a800b9e937e0271e8e93f4fb
Sam Zagrobelny <strangesast@gmail.com>
2020-04-02 15:41:38 -0400

    progress

--- client/src/weekly.ts
+++ client/src/weekly.ts
@@ -32,58 +32,6 @@ const args = {top: weekRowStep * weekRows};
 window.scrollTo(args);
 window.onbeforeunload = () => window.scrollTo(args);
 
-fromEvent(window, 'scroll').pipe(
-  throttleTime(100),
-  map(() => [window.scrollY, window.scrollY + window.innerHeight].map(g => yScale.invert(g))),
-  switchMap(range => worker.getWeeklyGraphData(range)),
-  map(items => group(items as any[], d => d3.timeWeek.floor(d.date).toISOString().slice(0, 10))),
-).subscribe((filteredData: Map<string, any[]>) => {
-
-  const a = d3.timeWeek.floor(new Date());
-  const b = d3.timeMinute.offset(d3.timeWeek.offset(a, 1), 15);
-  const domain = d3.timeMinute.every(15).range(a, b).map((d, i) => ((d.getDay() * 24 * 60 + d.getHours() * 60 + d.getMinutes()) / 15).toString());
-  console.log(domain);
-  const bandScale = d3.scaleBand()
-    .domain(domain)
-    .range([margin.left, width - margin.right])
-    .padding(0.1);
-
-  // const maxCount = d3.max(Array.from(filteredData.values()).reduce((a, b) => a.concat(b), []), (d: any) => d.count)
-  const xScale = d3.scaleLinear().domain([0, 10]).range([0, weekRowHeight]);
-
-  const data = Array.from(filteredData.entries())
-  svg.selectAll('g').data(data, ([key, value]) => key).join(
-    enter => enter.append('g')
-      // .call(s => s.append('rect').attr('height', weekRowHeight).attr('width', 400))
-      .attr('transform', ([key, value]) => `translate(0,${yScale(new Date(key))})`)
-    , update => update
-    , exit => exit.remove(),
-  )
-    .selectAll('rect')
-    .data(([key, value]) => value, (d: any) => d.date.toISOString())
-    .join(
-      enter => enter.append('rect')
-        .attr('x', (d: any, i) => {
-          const v = ((d.date.getDay() * 24 * 60 + d.date.getHours() * 60 + d.date.getMinutes()) / 15).toString();
-          return bandScale(v);
-        })
-        .attr('height', (d: any) => xScale(d.count))
-        .attr('y', (d: any) => weekRowHeight - xScale(d.count))
-        .attr('width', bandScale.bandwidth()).on('mouseenter', d => console.log(d)),
-      update => update,
-      exit => exit.remove(),
-    )
-
-});
-
-const data = [];
-for (let i = -52; i < bottomPadding / weekRowStep; i++) {
-  const date = d3.timeWeek.offset(week, i);
-  const id = date.toISOString().slice(0, 10);
-  data.push({date, id});
-}
-
-
 (async function() {
   let now;
   if (GENERATE_MOCKING) {
@@ -95,10 +43,42 @@ for (let i = -52; i < bottomPadding / weekRowStep; i++) {
   } else {
     now = new Date();
   }
+  fromEvent(window, 'scroll').pipe(
+    map(() => [window.scrollY, window.scrollY + window.innerHeight].map(g => yScale.invert(g))),
+    map(([minDate, maxDate]) => [d3.timeWeek.floor(minDate), d3.timeWeek.ceil(maxDate)]),
+    throttleTime(200),
+    switchMap(range => worker.getWeeklyGraphData(range)),
+    map(items => group(items as any[], d => d3.timeWeek.floor(d.date).toISOString().slice(0, 10))),
+  ).subscribe((filteredData: Map<string, any[]>) => {
+    const xScale = d3.scaleLinear().domain([0, 10]).range([weekRowHeight, 0]);
+    const line = d3.line().curve(d3.curveStep).y((d: any) => xScale(d.count))
+  
+    const data = Array.from(filteredData.entries()).map(([key, value]) => {
+      const [y, m, d] = key.split('-').map(s => +s);
+      const date = new Date(y, m-1, d);
+      return [date, value];
+    });
+    svg.selectAll('g').data(data, ([key, value]) => key).join(
+      enter => enter.append('g')
+        // .call(s => s.append('rect').attr('height', weekRowHeight).attr('width', 400))
+        .attr('transform', ([key, value]) => `translate(0,${yScale(key as any)})`)
+        .call(s => s.append('path')
+          .attr('d', ([key, value]: any) => {
+            const domain = [key, d3.timeWeek.offset(key, 1)];
+            const scale = d3.scaleTime()
+              .domain(domain)
+              .range([margin.left, width - margin.right]);
+            value.unshift({...value[0], count: 0})
+            value.push({...value[value.length - 1], count: 0})
+            return line.x((d: any) => scale(d.date))(value);
+          })
+          .attr('fill', 'lightgrey')
+          .attr('stroke', 'black')
+        )
+      , update => update
+      , exit => exit.remove(),
+    ).sort((a, b) => d3.ascending(a[0] as any, b[0] as any));
+});
 
-  const minDate = d3.timeWeek.offset(d3.timeWeek.floor(new Date()), -2);
-  const content = await worker.getWeeklyGraphData([minDate, d3.timeWeek.offset(minDate, 1)]);
-  console.log(content);
-  // d3.group(data, d => dkj:t
-  // console.log('data', content);
+
 })();
